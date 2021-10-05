8323a5101ae9c790581c196580a1e5f82be03a98
Emma Egstad <emma@egstad.com>
2021-10-05 14:13:41 -0700

    added animation to answers upon submit

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const getRandomNumber = (max) => {
     return Math.floor(Math.random() * max);
 };
 
+const onAnimationEnd = (ev) => {
+    ev.target.classList.remove('fade-in');
+};
+
 submitButton.addEventListener('click', () => {
     const randomIdx = getRandomNumber(cageQuotes.length);
     answer.textContent = cageQuotes[randomIdx];
+    answer.classList.add('fade-in');
 });
+
+answer.addEventListener('animationend', onAnimationEnd);
+
