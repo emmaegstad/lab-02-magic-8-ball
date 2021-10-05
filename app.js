bc317b1640e0668177f001f0167c7a08256e3eda
Emma Egstad <emma@egstad.com>
2021-10-05 13:32:13 -0700

    created JS functionality to display a random quote upon submitting question

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-  //math.floor(math.random()*array.length)
+import { cageQuotes } from './quotes.js';
+
+const submitButton = document.querySelector('.question-button');
+const answer = document.querySelector('.answer');
+
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+};
+
+submitButton.addEventListener('click', () => {
+    const randomIdx = getRandomNumber(cageQuotes.length);
+    answer.textContent = cageQuotes[randomIdx];
+});
